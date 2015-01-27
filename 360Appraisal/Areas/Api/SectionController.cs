c871b1533ed3b6cc9191ee26bdaa759943e21b65
Prasanth <prashanth702@gmail.com>
2015-01-26 23:21:28 -1200

    WebApi Implementation

---
+++ 360Appraisal/Areas/Api/SectionController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using _360Appraisal.Models;
+
+namespace _360Appraisal.Areas.Api
+{
+    public class SectionController : ApiController
+    {
+        private AppContext db = new AppContext();
+
+        // GET api/Section
+        public IQueryable<Section> GetSections()
+        {
+            return db.Sections;
+        }
+
+        // GET api/Section/5
+        [ResponseType(typeof(Section))]
+        public async Task<IHttpActionResult> GetSection(string id)
+        {
+            Section section = await db.Sections.FindAsync(id);
+            if (section == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(section);
+        }       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SectionExists(string id)
+        {
+            return db.Sections.Count(e => e.Key == id) > 0;
+        }
+    }
+}
---
+++ 360Appraisal/Areas/Api/TopicController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using _360Appraisal.Models;
+
+namespace _360Appraisal.Areas.Api
+{
+    public class TopicController : ApiController
+    {
+        private AppContext db = new AppContext();
+
+        // GET api/Topic
+        public IQueryable<Topic> GetTopics()
+        {
+            return db.Topics;
+        }
+
+        // GET api/Topic/5
+        [ResponseType(typeof(Topic))]
+        public async Task<IHttpActionResult> GetTopic(string id)
+        {
+            Topic topic = await db.Topics.FindAsync(id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(topic);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }       
+    }
+}
